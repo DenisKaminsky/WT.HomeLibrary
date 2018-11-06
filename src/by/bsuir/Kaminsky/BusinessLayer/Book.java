8562bbece81c4338bb293489830812d9517721df
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-06 15:36:55 +0300

    add Book class
    this is a model for Book

---
+++ src/by/bsuir/Kaminsky/BusinessLayer/Book.java
@@ -0,0 +1,38 @@
+package by.bsuir.Kaminsky.BusinessLayer;
+
+public class Book {
+	
+	private String author;
+	private String title;
+	private boolean isElectronic;
+	
+	public Book(String author,String title,boolean isElectronic){
+		this.author = author;
+		this.title = title;
+		this.isElectronic = isElectronic;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public void setAuthor(String value) {
+		author = value;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setTitle(String value) {
+		title = value;
+	}
+	
+	public boolean getIsElectronic(){
+		return isElectronic;
+	}
+	
+	public void setAuthor(Boolean value) {
+		isElectronic = value;
+	}
+}
