f0bf9a9216f7a70c5827d09a0419913f95d3ace4
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-06 15:47:37 +0300

    add user class
    this is a User model

---
+++ src/by/bsuir/Kaminsky/BusinessLayer/User.java
@@ -0,0 +1,38 @@
+package by.bsuir.Kaminsky.BusinessLayer;
+
+public class User {
+	
+	private String login;
+	private String password;
+	private boolean isAdministrator;
+	
+	public User(String login,String password,boolean isAdministrator){
+		this.login = login;
+		this.password = password;
+		this.isAdministrator = isAdministrator;
+	}
+	
+	public String getLogin(){
+		return login;
+	}
+	
+	public void setLogin(String value) {
+		login = value;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public void setPassword(String value) {
+		password = value;
+	}
+	
+	public boolean getIsAdministrator(){
+		return isAdministrator;
+	}
+	
+	public void setIsAdministrator(Boolean value) {
+		isAdministrator = value;
+	}
+}
